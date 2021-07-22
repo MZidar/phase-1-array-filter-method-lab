36524612713d261a25488e741f710da960841ed6
Michael Zidar <mzidar@gmail.com>
2021-07-22 12:46:17 -0400

    commit tree cd2e2b9013a3c3b8d34aecdc2f21fa327d53ce31

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+
+function findMatching(array, string)
+{return array.filter(function (str) {return str.toLowerCase() === string.toLowerCase(); });
+}
+
+
+
+
+function fuzzyMatch(array, string)
+{
+    let a = string.length;
+    return array.filter(function (str) {return str.slice(0,a) === string; });
+}
+
+function matchName(array, string)
+{
+    return array.filter(function (str) {return str.name === string; });
+}
